f20c582c936601a3ebc400ff4e6eb739f93065c8
brutal-art <david.s.csikai@gmail.com>
2025-05-08 07:54:51 +0200

    day01 tasks: distance + simiratiy score

--- 2024/day01.py
+++ 2024/day01.py
@@ -16,15 +16,22 @@ def parse_input(input_text: str):
         return left, right
 
 # write to file just in case [later list of filenames]
-def write_to_files(self, left_file="left.txt", right_file="right.txt"):
+def write_to_files(left, right, left_file="left.txt", right_file="right.txt"):
 
     with open(left_file, "w") as f_left:
-        for val in self.left:
+        for val in left:
             f_left.write(f"{val}\n")
 
     with open(right_file, "w") as f_right:
-        for val in self.right:
+        for val in right:
             f_right.write(f"{val}\n")
+
+def read_from_file(filenames):
+    columns = []
+    for i in filenames:
+        with open(i, 'r') as f:
+            columns.append([int(line.strip()) for line in f if line.strip()])
+    return columns
 
 # part one
 
@@ -36,13 +43,23 @@ def get_distance(left, right):
     left, right = sorted(left), sorted(right)
     return sum([ abs(i-j) for i, j in zip(left, right)])
 
+def get_similarity_score(left, right):
+
+    my_dict = {i:right.count(i) for i in left}
+    return sum([j*v for j, v in my_dict.items()])
+
+
 if __name__ == '__main__':
 
+    """
     session = input("Illeszd be az Advent of Code session azonosítód: ").strip()
     day = 1
-
-    handler = AoCInputHandler(session)
-
+    handler = AoCInputHandler(session)    
     l, r = parse_input(handler.fetch_input(day))
-    print(len(l), ' ', len(r))
+    """
+    columns = read_from_file(["left.txt", "right.txt"])
+    l, r = columns[0], columns[1]
+    # part one
     print(f'The sum of the distance of the two lists: {get_distance(l, r)}')
+    # part two
+    print(f'The similarity score of the lists: {get_similarity_score(l, r)}')
